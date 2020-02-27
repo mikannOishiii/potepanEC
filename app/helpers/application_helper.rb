0ddb289e5f9dfb13bfd1b0d4f3d7121ea0d36906
miki <mikannoishiii@gmail.com>
2020-02-27 16:03:14 +0900

    動的なタイトルに変更

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+  # 動的なタイトル
+  def full_title(page_title = '')
+    base_title = "BIGBAG Store"
+    if page_title.empty?
+      base_title
+    else
+      page_title + " - " + base_title
+    end
+  end
 end
