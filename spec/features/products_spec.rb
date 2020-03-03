357cc41bde0deb3fc163560d5e0a2ee0883a0a34
miki <mikannoishiii@gmail.com>
2020-03-03 16:01:39 +0900

    applicationhelperをrspecにinclude

--- spec/features/products_spec.rb
+++ spec/features/products_spec.rb
@@ -6,9 +6,9 @@ RSpec.feature "Products", type: :feature do
   # 商品が表示される
   scenario "動的に商品情報が表示される" do
     visit potepan_root_path
-    expect(page.title).to eq "POTEPAN BIGBAG Store"
+    expect(full_title("")).to eq "POTEPAN BIGBAG Store"
     visit potepan_product_path(product.id)
-    expect(page.title).to eq "#{product.name} - POTEPAN BIGBAG Store"
+    expect(full_title(product.name)).to eq "#{product.name} - POTEPAN BIGBAG Store"
     expect(page).to have_content "#{product.name}"
     expect(page).to have_content "#{product.display_price}"
     expect(page).to have_content "#{product.description}"
