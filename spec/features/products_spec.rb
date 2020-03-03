0b6bcdb0d03c413d16fb835430739dbbcfc278c9
miki <mikannoishiii@gmail.com>
2020-03-03 15:15:10 +0900

    title動的表示のテスト追加

--- spec/features/products_spec.rb
+++ spec/features/products_spec.rb
@@ -4,9 +4,11 @@ RSpec.feature "Products", type: :feature do
   let(:product) { FactoryBot.create(:product) }
 
   # 商品が表示される
-  scenario "show a product" do
+  scenario "動的に商品情報が表示される" do
+    visit potepan_root_path
+    expect(page.title).to eq "POTEPAN BIGBAG Store"
     visit potepan_product_path(product.id)
-
+    expect(page.title).to eq "#{product.name} - POTEPAN BIGBAG Store"
     expect(page).to have_content "#{product.name}"
     expect(page).to have_content "#{product.display_price}"
     expect(page).to have_content "#{product.description}"
