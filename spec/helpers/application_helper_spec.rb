b9109061d3f3ad7eabc9d33c03dde010ed54f650
miki <pikkoro-pacemaker@hotmail.co.jp>
2020-03-05 00:23:24 +0900

    applicationhelper単体テスト追加

---
+++ spec/helpers/application_helper_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+RSpec.describe ApplicationHelper, type: :helper do
+  describe 'full title helper' do
+    it "ページタイトルがないときサイトタイトルのみを返す" do
+      expect(full_title("")).to eq "POTEPAN BIGBAG Store"
+    end
+    it "ページタイトルがあるときページタイトルとサイトタイトルを返す" do
+      expect(full_title("ページタイトル")).to eq "ページタイトル - POTEPAN BIGBAG Store"
+    end
+  end
+end
