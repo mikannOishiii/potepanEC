9fbd681a426875921fa258421e0b74b21fe471ce
miki <mikannoishiii@gmail.com>
2020-03-04 12:25:08 +0900

    products_controller_spec.rbのリファクタ(before)

--- spec/controllers/potepan/products_controller_spec.rb
+++ spec/controllers/potepan/products_controller_spec.rb
@@ -4,22 +4,17 @@ RSpec.describe Potepan::ProductsController, type: :controller do
   let(:product) { FactoryBot.create(:product) }
 
   describe 'GET #show' do
-    it "正常にレスポンスを返すこと" do
+    before do
       get :show, params: { id: product.id }
+    end
+
+    it "正常にレスポンスを返すこと" do
       expect(response).to have_http_status(:success)
     end
-    it "正しい商品情報を表示すること" do
-      get :show, params: { id: product.id }
-      expect(product.name).to eq product.name
-      expect(product.display_price).to eq product.display_price
-      expect(product.description).to eq product.description
-    end
     it "showテンプレートで表示されること" do
-      get :show, params: { id: product.id }
       expect(response).to render_template(:show)
     end
     it '@productが取得できていること' do
-      get :show, params: { id: product.id }
       expect(assigns(:product)).to eq product
     end
   end
