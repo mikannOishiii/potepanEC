7c1acb20b4b3c1539280627188842ed8dded6b17
miki <pikkoro-pacemaker@hotmail.co.jp>
2020-03-10 12:16:51 +0900

    頂いたコメントを受け修正-20200310

--- spec/features/products_spec.rb
+++ spec/features/products_spec.rb
@@ -6,9 +6,9 @@ RSpec.feature "Products", type: :feature do
   # 商品が表示される
   scenario "動的に商品情報が表示される" do
     visit potepan_root_path
-    expect(full_title("")).to eq "POTEPAN BIGBAG Store"
+    expect(page).to have_title "POTEPAN BIGBAG Store"
     visit potepan_product_path(product.id)
-    expect(full_title(product.name)).to eq "#{product.name} - POTEPAN BIGBAG Store"
+    expect(page).to have_title "#{product.name} - POTEPAN BIGBAG Store"
     within(:css, '.media-body') do
       expect(page).to have_content "#{product.name}"
       expect(page).to have_content "#{product.display_price}"
