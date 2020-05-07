9084fcfb81d0e4f5c0cd060e28ba64fa3dcf0e38
miki <pikkoro-pacemaker@hotmail.co.jp>
2020-05-08 00:37:17 +0900

    新着商品実装&テスト

--- app/models/potepan/product_decorator.rb
+++ app/models/potepan/product_decorator.rb
@@ -1,4 +1,8 @@
 module Potepan::ProductDecorator
+  def self.prepended(base)
+    base.scope :new_available, -> { base.order(available_on: :desc) }
+  end
+
   def order_filtered_related_products
     # shuffle: 同じ商品ばかり並ばないように配列をshuffle
     # partition: 所属カテゴリが100%マッチするものは配列の先頭に移動
--- spec/controllers/potepan/static_pages_controller_spec.rb
+++ spec/controllers/potepan/static_pages_controller_spec.rb
@@ -2,9 +2,28 @@ require 'rails_helper'
 
 RSpec.describe Potepan::StaticPagesController, type: :controller do
   describe "GET #home" do
-    it "returns http success" do
+    let!(:new_products) { create_list(:product, 8, available_on: 2.day.ago) }
+    let!(:latest_product) { create(:product, available_on: 1.day.ago) }
+
+    before do
       get :home
+    end
+
+    it "returns http success" do
       expect(response).to have_http_status(:success)
+    end
+
+    it "homeテンプレートで表示されること" do
+      expect(response).to render_template(:home)
+    end
+
+    it "新着商品が8個取得できていること" do
+      expect(assigns(:new_products).length).to eq 8
+    end
+
+    it "新着商品が新着順に取得できていること" do
+      expect(assigns(:new_products).first).to eq latest_product
+      expect(assigns(:new_products).from(1)).to eq new_products[0..6]
     end
   end
 end
